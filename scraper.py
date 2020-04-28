375a59cbcc40113cfa391bc6fb0f84a616736e76
Gabriel Fuentes <gabrielf9024@gmail.com>
2020-04-27 22:19:12 -0700

    major changes

--- scraper.py
+++ scraper.py
@@ -21,40 +21,31 @@ stop_words = {'about','above','after','again','against','all','am','an','and','a
 html_junk = {'http', 'https', 'function', 'return', 'important','var', 'ariel', 'emoji', 'tex2jax'} # i will add more
 
 site_dict = dict()
-word_dict = dict()
+sub_list = list()
 freq = {}
-largest_url = (url,0)
+largest_url = ('',0)
 
 # This is the master MinHash that all documents are added into / checked against
 # We change the threshold to be the percent minimum a text needs to match
 # in similarity in order to be deemed 'too similar'
-lsh = MinHashLSH(threshold=0.9, num_perm=128)
-site_count = 0
+#lsh = MinHashLSH(threshold=0.9,num_perm=5000)
+#site_count = 0
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    global site_count
+    #global lsh
     frontier_list = list()
 
     #fetches the robots.txt
     if check_if_valid(url):
         robot_link = get_link_robot(url);
         robot = Robots.fetch(robot_link);
-        if (resp.status >= 200 and resp.raw_response != None):
+        if (resp.status == 200 and resp.raw_response != None):
             soup = BeautifulSoup(resp.raw_response.content,"html.parser")
-
-            # End the function if the url content is too similar to past urls
-            # If it is unique enough, add it to our knowledge base
-            site_count += 1
-            if too_similar(soup.get_text()):
-                return list()
-            else:
-                lsh.insert('url-' + str(site_count), soup.get_text())
-                # The above line inserts a new url content MinHash into our knowledge base
-                # They are labeled 'url-#'
-
             extract_tokens(url,soup)
             for link in soup.find_all('a'):
                 if link.get('href') != None and robot.allowed(link.get('href'),'IR S20 33805012,43145172,61658242'):
@@ -62,14 +53,14 @@ def extract_next_links(url, resp):
 
     return frontier_list
 
-def extract_tokens(url,soup):
-    # freq = {}
-    # token_file = open('tokens.txt','r')
-    # for line in token_file: # works ok, i will try to make it better
-    #     t,c = line.split()
-    #     freq[t] = int(c)
-    # token_file.close()
-
+def extract_tokens(url,soup):    
+    global freq
+    global stop_words
+    global largest_url
+    global html_junk
+    global site_dict
+    global sub_list
+    
     raw = soup.get_text()
     all_tokens = word_tokenize(raw)
     size = 0
@@ -95,15 +86,26 @@ def extract_tokens(url,soup):
     token_file = open('tokens.txt','w')
 
     counter = 1
+    sites_num = 0
+    subd_list = []
+    for key in site_dict.keys():
+        sites_num += site_dict[key]
+
+    if len(sub_list) > 0:
+        for keys in sub_list:
+            subd_list.append((keys,site_dict[keys]))
+            
+    token_file.write("sites crawled: " + str(sites_num)+ "\n")
+    token_file.write("sub domains: " + str(sorted(subd_list))+ "\n")
     for t,c in descending: # writes token freq in txt file
         token_file.write("{}. {} {}\n".format(counter,t,c))
         counter += 1
     token_file.close()
     
 # Returns the link of the Robots.txt as a string
-def get_link(url):
+def get_link_robot(url):
     parsed = urlparse(url)
-    return parsed.netloc + "/robots.txt";
+    return parsed.scheme +'://' + parsed.netloc + "/robots.txt";
 
 
 
@@ -113,7 +115,7 @@ def too_similar(text1: str) -> bool:
     data1 = re.split(r'\W+', text1)
 
     # Create the MinHash using the text
-    mh = MinHash(num_perm=128)
+    mh = MinHash(num_perm=5000)
     for word in data1:
         mh.update(word.encode('utf8'))
 
@@ -127,6 +129,8 @@ def too_similar(text1: str) -> bool:
 
 #Checks if valid uci link and not a trap
 def check_if_valid(url):
+    global site_dict
+    global sub_list
     parsed = urlparse(url)
 
     is_uci = False
@@ -137,25 +141,24 @@ def check_if_valid(url):
     if(urlRegex.search(parsed.netloc) != None):
         is_uci = True
 
-    SubRegex = re.compile('(?:([a-zA-z]+[.]{0,1})*((ics.uci.edu)|(cs.uci.edu)|(informatics.uci.edu)|(stat.uci.edu)|(today.uci.edu/department/information_computer_sciences)))')
+    SubRegex = re.compile('\b(?!uci|edu|informatics|cs|stat|today|www)\b\w+.')
     if(SubRegex.search(parsed.netloc) != None):
         is_sub = True
+        sub_list.append(parsed.netloc)
         
-    pathRegex = re.compile('(/.+?)(?:\1)+|(\?|&)[\w\W]+|(calendar)')
+    pathRegex = re.compile('(calendar|page|\d{4}/\d{2}|#\w+)')
     if(pathRegex.search(parsed.path) == None and parsed.query == ''):
         is_trap = False
         
     valid = (is_uci and is_trap == False)
-    
+    #########################################################################################################
+    # i think its better to have a tuple (int (frequency),list of paths(/jhsjdfhjh/akdjhfkjla), subdomain (true or false) ))# site_dict[parsed.netloc] = (1,[parsed.path], false)
     if valid == True:
         try:
-            site_dict[parsed.netloc] = (1,[parsed.path],is_sub)
+            site_dict[parsed.netloc] = 1
         except:
-            if parsed.path not in site_dict[parsed.netloc][1]:
-                site_dict[parsed.netloc][0]++
-                site_dict[parsed.netloc][1].append(parsed.path)
-            else:
-                valid = False
+            site_dict[parsed.netloc] +=1
+    #################################################################################################################
 
     return valid
         
